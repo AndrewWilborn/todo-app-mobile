4f6a0e8c6bd09c5db32ede27efbdf76e83c7f5ea
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-11 17:08:01 -0400

    ADDED keyboardavoidingview

--- app/Login.jsx
+++ app/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Center, Box, Heading, VStack, FormControl, Input, Button } from "native-base";
+import { Center, Box, Heading, VStack, FormControl, Input, Button, KeyboardAvoidingView } from "native-base";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./fbConfig"
 
@@ -23,23 +23,25 @@ export default function Login({setUser}) {
         <Heading mt={1} color="coolGray.400" fontWeight="medium" size="xs">
           Sign in to continue...
         </Heading>
-        <VStack space={3} mt={5}>
-          <FormControl isRequired>
-            <FormControl.Label color="coolGray.300">
-              Email
-            </FormControl.Label>
-            <Input onChangeText={setEmail} size="lg" color="coolGray.200" keyboardType="email-address" placeholder="example@email.com" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormControl.Label color="coolGray.300">
-              Password
-            </FormControl.Label>
-            <Input onChangeText={setPassword} size="lg" color="coolGray.200" type="password" />
-          </FormControl>
-          <Button onPress={handleLogin} mt={3} colorScheme="primary">
-            Sign In
-          </Button>
-        </VStack>
+        <KeyboardAvoidingView h={{ base: "400px", lg: "auto"}} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+          <VStack space={3} mt={5}>
+            <FormControl isRequired>
+              <FormControl.Label color="coolGray.300">
+                Email
+              </FormControl.Label>
+              <Input onChangeText={setEmail} size="lg" color="coolGray.200" keyboardType="email-address" placeholder="example@email.com" />
+            </FormControl>
+            <FormControl isRequired>
+              <FormControl.Label color="coolGray.300">
+                Password
+              </FormControl.Label>
+              <Input onChangeText={setPassword} size="lg" color="coolGray.200" type="password" />
+            </FormControl>
+            <Button onPress={handleLogin} mt={3} colorScheme="primary">
+              Sign In
+            </Button>
+          </VStack>
+        </KeyboardAvoidingView>
       </Box>
     </Center>
   );
