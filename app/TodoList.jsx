dd150fab6375ba255e121c5ab7d113afa3ad10e6
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-11 15:22:15 -0400

    UPDATED onchange for checkmark

--- app/TodoList.jsx
+++ app/TodoList.jsx
@@ -15,6 +15,25 @@ export default function TodoList({user}) {
     }
   }, [user])
 
+  const handleItemUpdate = (id, done) => {
+    const itemUpdate = {id, done: !done}
+
+    console.log(itemUpdate)
+
+    fetch("https://todo-c11.web.app/tasks/", {
+      method: 'PATCH',
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(itemUpdate)
+    })
+    .then(() => {
+      fetch(`https://todo-c11.web.app/tasks/${user.uid}`)
+        .then(res => res.json())
+        .then(setTodoItems)
+    }).catch(alert)
+  }
+
   return (
     <Center w="100%">
       <Box maxW={300} w="100%">
@@ -22,14 +41,26 @@ export default function TodoList({user}) {
           <TodoHeader user={user} setTodoItems={setTodoItems}/>
           {!todoItems
             ? <Text fontSize="lg" color="coolGray.300" textAlign="center">Loading...</Text>
-            : todoItems.map(item => (
+            : todoItems.map(item => {
+              const thisItemId = item.id
+              const thisItemDone = item.done
+              return(
               <HStack key={item.id} w="100%" justifyContent="space-between" alignItems="center">
                 <Checkbox
                   aria-label={item.title} 
-                  isChecked={item.done}/>
-                <Text fontSize={18} mx={2} strikeThrough={item.done} color={item.done ? "coolGray.500" : "coolGray.100"} textAlign="left" width="100%">{item.title}</Text>
+                  //defaultValue={item.done}
+                  isChecked={item.done}
+                  onChange={() => handleItemUpdate(thisItemId, thisItemDone)}/>
+                <Text 
+                fontSize={18} 
+                mx={2} 
+                strikeThrough={item.done} 
+                color={item.done ? "coolGray.500" : "coolGray.100"} 
+                textAlign="left" width="100%">
+                  {item.title}
+                </Text>
               </HStack>
-            ))
+            )})
           }
         </VStack>
       </Box>
